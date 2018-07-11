17a3436130c6ad6bafe144e3b58f9d203327ce88
Jeff Lieu <jefflieu@gmail.com>
2018-07-11 15:36:28 -0700

    Added routes and nav bar

---
+++ src/components/about.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+export default props =>{
+    return(
+        <div>
+            <h1 className="text-center">About Us</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium amet atque debitis deserunt dolorem enim error exercitationem illo laboriosam nisi nulla perferendis quae quos, repellat repudiandae ut, velit veniam voluptate?</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores aut eius exercitationem illum modi mollitia nisi odio odit officiis perspiciatis quas, qui quos soluta, sunt temporibus. Ipsum laboriosam libero officia.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium amet atque debitis deserunt dolorem enim error exercitationem illo laboriosam nisi nulla perferendis quae quos, repellat repudiandae ut, velit veniam voluptate?</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores aut eius exercitationem illum modi mollitia nisi odio odit officiis perspiciatis quas, qui quos soluta, sunt temporibus. Ipsum laboriosam libero officia.</p>
+        </div>
+    )
+}
---
+++ src/components/secret_list.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+
+export default props =>{
+    return(
+        <div>
+            <h1 className="text-center">Top Secret</h1>
+            <ul>
+                <li>Cloud</li>
+                <li>Luca</li>
+                <li>Noctis</li>
+                <li>Sephoroth</li>
+            </ul>
+        </div>
+    );
+}
