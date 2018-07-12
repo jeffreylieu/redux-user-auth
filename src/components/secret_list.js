f7e1ed42b5ea6f1c45d64fd8b5b31efb95fb1d16
Jeff Lieu <jefflieu@gmail.com>
2018-07-11 18:02:14 -0700

    Added auth and non secret page

--- src/components/secret_list.js
+++ src/components/secret_list.js
@@ -2,7 +2,8 @@ import React from 'react';
 
 
 
-export default props =>{
+
+const Secret = props =>{
     return(
         <div>
             <h1 className="text-center">Top Secret</h1>
@@ -11,7 +12,12 @@ export default props =>{
                 <li>Luca</li>
                 <li>Noctis</li>
                 <li>Sephoroth</li>
+                <li>Bob</li>
+                <li>Lou</li>
+                <li>Cletus</li>
             </ul>
         </div>
     );
 }
+
+export default Secret;
